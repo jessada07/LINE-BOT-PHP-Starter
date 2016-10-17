f079c60c8112769119592c94cadda075cb755a0e
jessada07 <jessada_drummer@hotmail.com>
2016-10-17 23:07:21 +0700

    fix

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'บ่นไรไอ้ฟาย',
+				'text' => 'บ่นไรไอ้ฟาย'
                 'text' => 'GG EZ'
 			];
 
