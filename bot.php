90e5bc7c4ef1e0bef979d492cfed616a1ce52d34
jessada07 <jessada_drummer@hotmail.com>
2016-10-22 15:11:58 +0700

    fix

--- bot.php
+++ bot.php
@@ -13,6 +13,8 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
+			// Get replyToken
+	        $replyToken = $event['replyToken'];
 			if($text == "Hello"){
 				// Build message to reply back
 				$messages = [
@@ -23,9 +25,7 @@ if (!is_null($events['events'])) {
 		}
 	}
 }
-function reply() {
-	// Get replyToken
-	$replyToken = $event['replyToken'];
+function reply() {	
     // Make a POST Request to Messaging API to reply to sender
 	$url = 'https://api.line.me/v2/bot/message/reply';
 	$data = [
