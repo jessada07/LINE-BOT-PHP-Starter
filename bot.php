bae286137809374fcc76dca96cbe7ed1dd646725
jessada07 <jessada_drummer@hotmail.com>
2016-10-22 15:46:54 +0700

    fix

--- bot.php
+++ bot.php
@@ -50,6 +50,7 @@ if (!is_null($events['events'])) {
 							     'text' => $text		
 							    ];
 					break;
+			}
 				// Make a POST Request to Messaging API to reply to sender
 	            $url = 'https://api.line.me/v2/bot/message/reply';
 	            $data = [
