42e4b234a30152e8d7420439bf0c70089b928742
jessada07 <jessada_drummer@hotmail.com>
2016-10-27 19:33:15 +0700

    s

--- bot.php
+++ bot.php
@@ -1,88 +1,35 @@
-<?php
+<html>
+  <head>
+    <?php
 $verz="1.0";
 exec("mode COM3: BAUD=9600 PARITY=n DATA=8 STOP=1 to=off dtr=off rts=off"); 
-$fp = fopen("COM3", "w");
-fwrite($fp, "1");
-$access_token = 'W+X36trYjmT3J3MwxGH0eVwYFEiJIN/MUhRKS4NkOAVjMjS1iy43ja//nWUu3/sVjyDheG3kYnZS23ZGunisgNyCs86RynE/NclW0ibHkFoiIJKrnqrIL4ean0c7rvDYAWx+JzG5yv/cvfuzze0G6QdB04t89/1O/w1cDnyilFU=';
+$fp = fopen("COM3", "w"); 
 
-// Get POST body content
-$content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['message']['text'];
-			// Get replyToken
-	        $replyToken = $event['replyToken'];
-    
-			switch($text){
-				case 'เปิดไฟ':
-				    // Build message to reply back           
-				    $messages = [
-							     'type' => 'text',
-							     'text' => 'เรียบร้อย'			
-							    ];                      
-				    break;
-				case 'ปิดไฟ':
-				    // Build message to reply back
-            fwrite($fp, "0");
-				    $messages = [
-							     'type' => 'text',
-							     'text' => 'เรียบร้อย'			
-							    ];            
-				    break;
-				case 'อุณหภูมิ':
-				    // Build message to reply back
-				    $messages = [
-							     'type' => 'text',
-							     'text' => 'ขณะนี้อุณหภูมิ'			
-							    ];
-				    break;
-  			    default:
-					$messages = [
-							     'type' => 'text',
-							     'text' => $text		
-							    ];
-					break;
-			}
-		}
-    fclose($fp);  
-		if ($event['type'] == 'message' && $event['message']['type'] == 'sticker'){
-			// Get text sent
-			$text = $event['message']['sticker'];
-			// Get replyToken
-	        $replyToken = $event['replyToken'];
-			$random = rand(407,430);
-			$messages = [
-						 'type' => 'sticker',
-                         'packageId' => '1',
-                         'stickerId' => $random
-						];
-		}			
-		// Make a POST Request to Messaging API to reply to sender
-	    $url = 'https://api.line.me/v2/bot/message/reply';
-	    $data = [
-		        'replyToken' => $replyToken,
-			    'messages' => [$messages]
-			    ];
-	    $post = json_encode($data);
-		$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+switch($_POST)
+	{
+		case isset($_POST['submitOn']):
+			fwrite($fp, "1"); //write string to serial  // Turn On LED 1
+			break;
+		case isset($_POST['submitOff']):
+			fwrite($fp, "0"); //write string to serial  // Turn Off LED 1
+			break;
 
-		$ch = curl_init($url);
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-		$result = curl_exec($ch);
-		url_close($ch);
-
-		echo $result . "\r\n";			
 	}
-}
+fclose($fp);
 ?>
+  </head>
+
+  <body>
+
+    <h1>Control Panel</h1>
+
+    <form  method="post" action=""
+      <?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+      <input type='submit' name='submitOn' value='Turn LED 1 ON'>
+        <input type='submit' name='submitOff' value='Turn LED 1 OFF'>
+
+	</form>
+
+  </body>
+
+</html>
