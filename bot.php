9cc93a4e6922913a02a160646f0a0068e40801d2
jessada07 <jessada_drummer@hotmail.com>
2016-11-09 23:27:52 +0700

    ljh

--- bot.php
+++ bot.php
@@ -61,12 +61,12 @@ if (!is_null($events['events'])) {
             curl_close( $curl_handle ); 
             $obj = json_decode($text);
             $mes = $obj->{'field1'};
-            &temp = "ขณะนี้อุณหภูมิ  .$mes. &deg;C";
+            &temp = "ขณะนี้อุณหภูมิ  .$mes. ";
             
 				    // Build message to reply back
 				    $messages = [
 							     'type' => 'text',
-							     'text' => $mes
+							     'text' => $temp
 							    ];
             
 				    break;
