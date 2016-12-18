285a02cfb66cc7121bcbeb0e1dc5010f864dda28
jessada07 <jessada_drummer@hotmail.com>
2016-12-18 16:17:56 +0700

    asd

--- bot.php
+++ bot.php
@@ -132,7 +132,7 @@ if (!is_null($events['events'])) {
                curl_close( $curl_handle ); 
                $object = json_decode($text, TRUE);
                $name = $object['result']['name']; 
-               $addname .= $name."\n";
+               $addname .= "->>".$name."\n";
             }            
 				    // Build message to reply back
 				    $messages = [
