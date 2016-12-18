8aa35d4d1ba98217047c94ae9425609ccb8d08e9
jessada07 <jessada_drummer@hotmail.com>
2016-12-18 15:06:25 +0700

    re

--- bot.php
+++ bot.php
@@ -121,7 +121,7 @@ if (!is_null($events['events'])) {
             $text = curl_exec( $curl_handle );
             curl_close( $curl_handle ); 
             $obj = json_decode($text, TRUE);
-            $mes = $obj['results']['place_id']; 
+            $mes = $obj['results']['place_id'][0]; 
             
             
 				    // Build message to reply back
