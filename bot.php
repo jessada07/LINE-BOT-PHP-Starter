ce570aef45a16ef60cbb0c7a49c0227390923a0b
jessada07 <jessada_drummer@hotmail.com>
2016-12-18 20:47:29 +0700

    sdsda

--- bot.php
+++ bot.php
@@ -133,7 +133,8 @@ if (!is_null($events['events'])) {
                $object = json_decode($text, TRUE);
                $name = $object['result']['name']; 
                $number = $object['result']['formatted_phone_number'];
-               $addname .= "->>".$name."\n".$number."\n\n";
+               $address = $object['result']['formatted_address'];
+               $addname .= "->>".$name."\n".$number."\n".$address."\n\n";
             }            
 				    // Build message to reply back
 				    $messages = [
