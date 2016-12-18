13eb7dde5dc230fa4f574ea8b265473cb3672f18
jessada07 <jessada_drummer@hotmail.com>
2016-12-18 16:05:46 +0700

    asdas

--- bot.php
+++ bot.php
@@ -6,25 +6,6 @@ $access_token = 'W+X36trYjmT3J3MwxGH0eVwYFEiJIN/MUhRKS4NkOAVjMjS1iy43ja//nWUu3/s
 $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
-function familyName() {
-      // Make a POST Request to Messaging API to reply to sender
-	    $url = 'https://api.line.me/v2/bot/message/reply';
-	    $data = [
-		        'replyToken' => $replyToken,
-			    'messages' => [$messages]
-			    ];
-	    $post = json_encode($data);
-		$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-		$ch = curl_init($url);
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-		$result = curl_exec($ch);
-		url_close($ch);
-    echo $result . "\r\n";	
-}
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
 	// Loop through each event
@@ -157,8 +138,7 @@ if (!is_null($events['events'])) {
 							     'type' => 'text',
 							     'text' => "$name"
 							    ];
-            familyName();  
-				    break;
+          break;
   			  default:
 					$messages = [
 							     'type' => 'text',
