3e33d08e001a47303e11d5d7a86127f611e181fd
jessada07 <jessada_drummer@hotmail.com>
2016-12-18 20:37:59 +0700

    asd

--- bot.php
+++ bot.php
@@ -136,13 +136,8 @@ if (!is_null($events['events'])) {
             }            
 				    // Build message to reply back
 				    $messages = [
-                  { 
 							     'type' => 'text',
 							     'text' => "$addname"
-                  }, {
-                    "type" => "text",
-                    "text" => "เชิญเลือกร้าน"
-                   }
 					 ];
           break;
   			  default:
