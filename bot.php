3f12c38cc21eb63936fd4892744f48888d36cadc
jessada07 <jessada_drummer@hotmail.com>
2016-12-18 15:38:50 +0700

    dsf

--- bot.php
+++ bot.php
@@ -149,6 +149,22 @@ if (!is_null($events['events'])) {
 							     'type' => 'text',
 							     'text' => "$name"
 							    ];
+        // Make a POST Request to Messaging API to reply to sender
+	    $url = 'https://api.line.me/v2/bot/message/reply';
+	    $data = [
+		        'replyToken' => $replyToken,
+			    'messages' => [$messages]
+			    ];
+	    $post = json_encode($data);
+		$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+		$result = curl_exec($ch);
+		url_close($ch);
     }
 		if ($event['type'] == 'message' && $event['message']['type'] == 'sticker'){
 			// Get text sent
