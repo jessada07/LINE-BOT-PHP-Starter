4c678730e599eedc966512aeb3daf17c2ee7a3f4
jessada07 <jessada_drummer@hotmail.com>
2017-03-31 11:11:06 +0700

    add message

--- bot.php
+++ bot.php
@@ -248,7 +248,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'welcome1'
+				'text' => 'beacon area!!!!'
 			];
 		}			
 		// Make a POST Request to Messaging API to reply to sender
