acd5860a9b51b775e0e88a3a0ae6b0c0c6cd898d
jessada07 <jessada_drummer@hotmail.com>
2017-04-02 23:18:06 +0700

    debug

--- bot.php
+++ bot.php
@@ -17,10 +17,10 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 	        $replyToken = $event['replyToken'];
 			// Build message to reply back
-			$messages = [
+			$messages = {
 				'type' => 'text',
 				'text' => 'leave beacon area!!!!'
-			];
+			};
 		}				
 		// Make a POST Request to Messaging API to reply to sender
 	    $url = 'https://api.line.me/v2/bot/message/reply';
