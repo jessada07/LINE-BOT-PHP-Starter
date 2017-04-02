a5c6251b023a8236e8330eca9c1ea92ab82098d6
jessada07 <jessada_drummer@hotmail.com>
2017-04-02 23:11:59 +0700

    debug

--- bot.php
+++ bot.php
@@ -15,10 +15,10 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$messages = {
+			$messages = [
 				'type' => 'text',
 				'text' => 'welcome'
-			};
+			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
