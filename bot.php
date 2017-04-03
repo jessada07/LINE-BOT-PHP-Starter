a26abc6728c258d60de45f5a5f9f2bfe51ceef96
jessada07 <jessada_drummer@hotmail.com>
2017-04-03 11:43:32 +0700

    debug

--- bot.php
+++ bot.php
@@ -14,7 +14,7 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
-      $idLine - $event['message']['id'];
+      $idLine = $event['message']['id'];
 			// Get replyToken
 	        $replyToken = $event['replyToken'];
 			// Build message to reply back
