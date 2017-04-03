2c6b3bfd8ccc49e32f61d48cff6f629fa688f09a
jessada07 <jessada_drummer@hotmail.com>
2017-04-03 11:22:23 +0700

    debug

--- bot.php
+++ bot.php
@@ -22,8 +22,6 @@ if (!is_null($events['events'])) {
 				'altText' => 'This is a buttons template',
 				'template' => array(
 					'type' => 'buttons',
-          'thumbnailImageUrl' => 'http://www.pngall.com/wp-content/uploads/2016/04/Discount-PNG-Images.png',
-          'title' => 'discount',
 					'text' => 'Please select',
 					'actions' => array(
               array(
