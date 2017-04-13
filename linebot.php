0c6a1155118309f220fd621865814595230d0c96
jessada07 <jessada_drummer@hotmail.com>
2017-04-13 14:17:06 +0700

    debug

--- linebot.php
+++ linebot.php
@@ -1,15 +1,8 @@
 <?php
 $access_token = 'W+X36trYjmT3J3MwxGH0eVwYFEiJIN/MUhRKS4NkOAVjMjS1iy43ja//nWUu3/sVjyDheG3kYnZS23ZGunisgNyCs86RynE/NclW0ibHkFoiIJKrnqrIL4ean0c7rvDYAWx+JzG5yv/cvfuzze0G6QdB04t89/1O/w1cDnyilFU=';
-// Get POST body content
-$content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'beacon' && $event['beacon']['type'] == 'enter') {
+
+$to ='U1afc8417a53546990d662f7319e981e6';
+
 			// Get text sent
 			$text = $event['message']['text'];      
 			// Get replyToken
@@ -20,7 +13,7 @@ if (!is_null($events['events'])) {
 				'altText' => 'This is a buttons template',
 				'template' => array(
 					'type' => 'buttons',
-					'text' => 'ระบบจองคิว',
+					'text' => 'Please select',
 					'actions' => array(
               array(
 							'type' => 'postback',
@@ -30,57 +23,30 @@ if (!is_null($events['events'])) {
 						  array(
 							'type' => 'message',
 							'label' => 'Help',
-              'text' => 'คุณสามารถจองคิวได้เฉพาะหน้าร้านเท่านั้นโดยกดคำว่า Order'
+              'text' => ' Help : คุณสามารถจองคิวได้เฉพาะหน้าร้านเท่านั้นโดยกดคำว่า Order'
 						  )
 					 )
 				 )
 			);
-		}			
-    if ($event['type'] == 'postback' && $event['postback']['data'] == 'order') {
-      // Get replyToken
-      $status = $event['beacon']['type']; 
-	    $replyToken = $event['replyToken'];
-      $url = "http://api.thingspeak.com/channels/202503/feeds/last.json?api_key=0QJTN9QPAXWCI68I";
-      $curl_handle = curl_init();
-      curl_setopt($ch1, CURLOPT_SSL_VERIFYPEER, false);
-      curl_setopt( $curl_handle, CURLOPT_URL, $url );
-      curl_setopt( $curl_handle, CURLOPT_RETURNTRANSFER, true);
-      $text = curl_exec( $curl_handle );
-      curl_close( $curl_handle ); 
-      $obj = json_decode($text);
-      $mes = $obj->{'field1'}; 
-      $mes = $mes + 1;
-      
-      $url = 'https://api.thingspeak.com/update?api_key=0QJTN9QPAXWCI68I&field1='.$mes;
-      $curl_handle = curl_init();
-      curl_setopt($ch1, CURLOPT_SSL_VERIFYPEER, false);
-      curl_setopt( $curl_handle, CURLOPT_URL, $url );
-      curl_setopt( $curl_handle, CURLOPT_RETURNTRANSFER, true);
-      curl_exec( $curl_handle );
-      curl_close( $curl_handle );
-      $messages = [
-          'type' => 'text',
-          'text' => 'Queue ของคุณคือ   '.$status
-      ];
-    }
-    
-		// Make a POST Request to Messaging API to reply to sender
-	    $url = 'https://api.line.me/v2/bot/message/reply';
-	    $data = [
-		        'replyToken' => $replyToken,
-			      'messages' => [$messages]
-			    ];
-	  $post = json_encode($data);
-		$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-		$ch = curl_init($url);
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-		$result = curl_exec($ch);
-		url_close($ch);
-		echo $result . "\r\n";		   
-	}
-}
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/push';
+			$data = [
+				'to' => $to,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+      echo "OK";
 ?>
