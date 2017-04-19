0c364c601221b04f0be4aed5a59d08be6ad5472d
jessada07 <jessada_drummer@hotmail.com>
2017-04-19 23:30:09 +0700

    debug

--- testprofile.php
+++ testprofile.php
@@ -1,25 +1,17 @@
 <?php
 $access_token = 'W+X36trYjmT3J3MwxGH0eVwYFEiJIN/MUhRKS4NkOAVjMjS1iy43ja//nWUu3/sVjyDheG3kYnZS23ZGunisgNyCs86RynE/NclW0ibHkFoiIJKrnqrIL4ean0c7rvDYAWx+JzG5yv/cvfuzze0G6QdB04t89/1O/w1cDnyilFU=';
-
+$secret = '3b44899e97cacf93240d4112b87ac873';
 $to ='U1afc8417a53546990d662f7319e981e6';
 
-	 // Make a POST Request to Messaging API to reply to sender
-	 $url = 'https://api.line.me/v2/bot/profile/'.$to;
-	 $data = [
-	  	'to' => $to,
-	    ];
-	  $post = json_encode($data);
-	  $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-	  $ch = curl_init($url);
-	  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-	  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	  curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-	  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-	  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-	  $result = curl_exec($ch);
-	  curl_close($ch);
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '$secret']);
+$response = $bot->getProfile($to);
+if ($response->isSucceeded()) {
+    $profile = $response->getJSONDecodedBody();
+    echo $profile['displayName'];
+    echo $profile['pictureUrl'];
+    echo $profile['statusMessage'];
+}
 
-	  echo $result . "\r\n";
-      echo "OK";
 ?>
