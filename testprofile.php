d46cc5047e34c8f52b068a994865a7a1cbde4f49
jessada07 <jessada_drummer@hotmail.com>
2017-04-19 23:36:32 +0700

    debug

--- testprofile.php
+++ testprofile.php
@@ -1,6 +1,7 @@
 <?php
+require __DIR__  . '/vendor/autoload.php';
 $access_token = 'W+X36trYjmT3J3MwxGH0eVwYFEiJIN/MUhRKS4NkOAVjMjS1iy43ja//nWUu3/sVjyDheG3kYnZS23ZGunisgNyCs86RynE/NclW0ibHkFoiIJKrnqrIL4ean0c7rvDYAWx+JzG5yv/cvfuzze0G6QdB04t89/1O/w1cDnyilFU=';
-$secret = '3b44899e97cacf93240d4112b87ac873';
+$secret ='3b44899e97cacf93240d4112b87ac873';
 $to ='U1afc8417a53546990d662f7319e981e6';
 
 
