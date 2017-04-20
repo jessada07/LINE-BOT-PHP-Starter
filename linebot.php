b429f7974b388bd173d454ec7aa28485f5206b00
jessada07 <jessada_drummer@hotmail.com>
2017-04-21 00:11:19 +0700

    กำิี

--- linebot.php
+++ linebot.php
@@ -2,8 +2,10 @@
     require __DIR__  . '/vendor/autoload.php';
     $access_token = 'W+X36trYjmT3J3MwxGH0eVwYFEiJIN/MUhRKS4NkOAVjMjS1iy43ja//nWUu3/sVjyDheG3kYnZS23ZGunisgNyCs86RynE/NclW0ibHkFoiIJKrnqrIL4ean0c7rvDYAWx+JzG5yv/cvfuzze0G6QdB04t89/1O/w1cDnyilFU=';
     $secret ='3b44899e97cacf93240d4112b87ac873';
-    $content = file_get_contents('php://input');
     
+    // Get POST body content
+    $content = file_get_contents('php://input');
+
     $url = "http://api.thingspeak.com/channels/202506/feeds/last.json?api_key=5WBJKUX2CGYQ04N2";
     $curl_handle = curl_init();
     curl_setopt($ch1, CURLOPT_SSL_VERIFYPEER, false);
@@ -14,10 +16,8 @@
     $obj = json_decode($text);
     $to = $obj->{'field2'}; 
 
-    $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-    $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $secret]);
-
-    $textMessageBuilder = array(
+   // Build message to reply back
+			$messages = array(
 				'type' => 'template',
 				'altText' => 'This is a buttons template',
 				'template' => array(
@@ -29,8 +29,22 @@
               'data' => 'cancel'
 					 )
 				 )
-		);
-    $response = $bot->pushMessage($to, json_encode($textMessageBuilder));
-
-    echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+			);
+      // Make a POST Request to Messaging API to reply to sender
+	    $url = 'https://api.line.me/v2/bot/message/reply';
+	    $data = [
+		        'replyToken' => $replyToken,
+			      'messages' => [$messages]
+			    ];
+	  $post = json_encode($data);
+		$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+		$result = curl_exec($ch);
+		url_close($ch);
+		echo $result . "\r\n";		 
 ?>
