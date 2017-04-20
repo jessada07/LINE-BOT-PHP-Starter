384ca4d20de49e0bbc558900e3c6b91d54e33300
jessada07 <jessada_drummer@hotmail.com>
2017-04-21 00:12:48 +0700

    debug

--- linebot.php
+++ linebot.php
@@ -31,9 +31,9 @@
 				 )
 			);
       // Make a POST Request to Messaging API to reply to sender
-	    $url = 'https://api.line.me/v2/bot/message/reply';
+	    $url = 'https://api.line.me/v2/bot/message/push';
 	    $data = [
-		        'replyToken' => $replyToken,
+		        'replyToken' => $to,
 			      'messages' => [$messages]
 			    ];
 	  $post = json_encode($data);
