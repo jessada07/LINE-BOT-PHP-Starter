a30b9c43725e3800cb00fa28faea1d372d528854
jessada07 <jessada_drummer@hotmail.com>
2017-04-20 23:52:56 +0700

    debug

--- linebot.php
+++ linebot.php
@@ -2,7 +2,8 @@
     require __DIR__  . '/vendor/autoload.php';
     $access_token = 'W+X36trYjmT3J3MwxGH0eVwYFEiJIN/MUhRKS4NkOAVjMjS1iy43ja//nWUu3/sVjyDheG3kYnZS23ZGunisgNyCs86RynE/NclW0ibHkFoiIJKrnqrIL4ean0c7rvDYAWx+JzG5yv/cvfuzze0G6QdB04t89/1O/w1cDnyilFU=';
     $secret ='3b44899e97cacf93240d4112b87ac873';
-
+    $content = file_get_contents('php://input');
+    
     $url = "http://api.thingspeak.com/channels/202506/feeds/last.json?api_key=5WBJKUX2CGYQ04N2";
     $curl_handle = curl_init();
     curl_setopt($ch1, CURLOPT_SSL_VERIFYPEER, false);
