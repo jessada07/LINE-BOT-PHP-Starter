8730cecad1b0139681703c4700401c6c034a7ff1
jessada07 <jessada_drummer@hotmail.com>
2017-04-21 11:08:03 +0700

    debug

--- bot.php
+++ bot.php
@@ -8,14 +8,6 @@ $events = json_decode($content, true);
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
-    /*if($event['type'] == 'beacon' && $check_order == '1') {
-      $replyToken = $event['replyToken'];
-      $messages = [
-          'type' => 'text',
-          'text' => 'success'
-      ];
-      $check_order = '0';
-    }*/
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'beacon' && $event['beacon']['type'] == 'enter') {
 			// Get text sent
@@ -46,7 +38,7 @@ if (!is_null($events['events'])) {
       $check_order = '1';
 		}			
     if ($event['type'] == 'postback' && $event['postback']['data'] == 'order') {
-      if($check_order =='1'){
+      if($check_order === '1'){
         // Get replyToken
         $user_id = $event['source']['userId'];
 	      $replyToken = $event['replyToken'];
