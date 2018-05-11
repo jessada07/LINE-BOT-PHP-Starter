a358f94d5192558335f471a44904592e51452727
jessada07 <jessada_drummer@hotmail.com>
2018-05-11 11:12:30 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,12 +1,29 @@
 <?php
-	$battery = $_GET['battery'];
+	$data = $_GET['data'];
     $access_token = 'W+X36trYjmT3J3MwxGH0eVwYFEiJIN/MUhRKS4NkOAVjMjS1iy43ja//nWUu3/sVjyDheG3kYnZS23ZGunisgNyCs86RynE/NclW0ibHkFoiIJKrnqrIL4ean0c7rvDYAWx+JzG5yv/cvfuzze0G6QdB04t89/1O/w1cDnyilFU=';
     
+    if($data = "Battery?"){
+    	$url = "http://api.thingspeak.com/channels/482888/feeds/last.json?api_key=5AZJRVINNBDSF7B7";
+        $curl_handle = curl_init();
+        curl_setopt($ch1, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt( $curl_handle, CURLOPT_URL, $url );
+        curl_setopt( $curl_handle, CURLOPT_RETURNTRANSFER, true);
+        $text = curl_exec( $curl_handle );
+        curl_close( $curl_handle ); 
+        $obj = json_decode($text);
+        $mes = $obj->{'field1'};             
+		    // Build message to reply back
+		    $messages = [
+					    'type' => 'text',
+				        'text' => 'ขณะนี้แบตเตอรี่ '.$mes.'%'
+					    ];
+    }else{
     // Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'Battery : '.$battery.'%'
+				'text' => 'Warning!!! Battery : '.$data.' %'
 			];
+    }
       // Make a POST Request to Messaging API to reply to sender
 	    $url = 'https://api.line.me/v2/bot/message/push';
 	    $data = [
